9d02d5e8db9ba487f0508b6b63b83063530fc7ec
Neide Souto <neilousouto@gmail.com>
2023-01-29 15:35:57 +0200

    initial commit

---
+++ src/pages/HelpComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class ="q-pa-lg">
+    <h5 class="q-at-none">Help</h5>
+    <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+    <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HelpComponent'
+})
+
+</script>
